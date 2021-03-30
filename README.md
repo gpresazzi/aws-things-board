863673799e89b6a324aaa117b4c63f9bd907608f
Giulio Presazzi <pgiulio@amazon.com>
2021-03-30 10:50:07 +0100

    Add duck dns script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# aws-things-board
-Aws infrastructure for ThingsBoard.io
+# Summary
+This package is used to build a basic AWS infrastructure with thingsboard.io deployed on a single EC2 instance.
+
+## Requirements
+- AWS CLI installed
+- AWS profile configured
+- SSM parameter for token id in DuckDNS
+
+
+## Execute
+
+```bash
+./deploy.sh
+```
+
+This script execute the following steps:
+
+1 - Create the infrastructure (single EC2 instance in VPC public reachable)
+2 - Run a command to initialize the host
--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,14 @@ aws cloudformation deploy --template-file ec2-vpc.yml --stack-name ec2-http-stac
 # wait some time before deploying script
 sleep 5
 
+# Retrieve instance ID
 instance_id=`aws ec2 describe-instances --filters "Name=tag-value,Values=instance-server" --query "Reservations[*].Instances[*].[InstanceId]" --output text`
+
+# Set parameter using SSM
+aws ssm put-parameter --name "duck-token-id" \
+    --description "Duck DNS token id" \
+    --value "1e595702-b998-4df6-835b-c0d3a0d48bf9" \
+    --type "String"
+
+# Send command using SSM
 aws ssm send-command --document-name "AWS-RunShellScript" --instance-ids "$instance_id" --cli-input-json file://install-thinger.json
